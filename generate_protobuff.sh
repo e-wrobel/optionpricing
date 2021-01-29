c2279f340cf98932be8c77c52b7bd129ae9f1871
Marcin <narcin.wroblewski@aol.com>
2021-01-29 22:30:12 +0100

    Added protos for python

--- generate_protobuff.sh
+++ generate_protobuff.sh
@@ -4,17 +4,30 @@
 echo "Generating subs from proto buffers. Entering protobuffers directory..."
 cd protobuffers
 protoc --go_out=plugins=grpc:. *.proto
-
 if [ $? -ne 0 ]; then
-  echo "Unable to generate stubs. Exiting..."
+  echo "Unable to generate Go stubs. Exiting..."
   exit 1
 fi
 
-echo "Stubs generated. Now I am going to move them to option directory..."
+echo "Go Stubs generated. Now I am going to move them to option directory..."
 mv *.go ../option/
 
 if [ $? -ne 0 ]; then
-  echo "Unable to generate stubs. Exiting..."
+  echo "Unable to move Go stubs. Exiting..."
+  exit 1
+fi
+
+protoc.exe  -I=. --python_out=. ./option.proto
+if [ $? -ne 0 ]; then
+  echo "Unable to generate Python stubs. Exiting..."
+  exit 1
+fi
+
+echo "Python Stubs generated. Now I am going to move them to option directory..."
+mv *.py ../option/
+
+if [ $? -ne 0 ]; then
+  echo "Unable to move Python stubs. Exiting..."
   exit 1
 fi
 
